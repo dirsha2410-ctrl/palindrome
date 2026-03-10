649dbfb4d6fb3e6cabcce253e271bb97d8bc1230
dirsha2410-ctrl <dirsha2410@gmail.com>
2026-03-10 21:31:37 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int start = 0;
+    int end = str.length() - 1;
+    bool isPalindrome = true;
+
+    while (start < end) {
+        if (str[start] != str[end]) {
+            isPalindrome = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+
+    if (isPalindrome) {
+        cout << "Palindrome " << endl;
+    } else {
+        cout << "not Palindrome" << endl;
+    }
+
+    return 0;
+}
